9e63b77bb3654b53854f7f773d0f595d44f42e5f
Sam Lijo <samuelvillalvalijo@gmail.com>
2021-06-11 18:09:55 -0500

    Playing around with styling

--- src/CounterButton/CounterButton.js
+++ src/CounterButton/CounterButton.js
@@ -13,20 +13,45 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 
+import Box from '@material-ui/core/Box';
+
 
 
 const useStyles = makeStyles((theme) =>({
   root: {
     display: "flex",
     justifyContent: "center",
+    alignItems: "center",
     flexGrow: 1,
-    maxWidth: 250,
+  
+    //maxWidth: 250,
+      // "background-color": "#26a69a",
+  
     
+  },
+  cardCss: {
+    display: "flex",
     "background-color": "#26a69a",
-    
+    width: 250,
+    height: 460,
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent:"center",
+    justifyContent: "space-around",
+    // boxShadow: '10',
+
+
+  },
+  countDisplay:{
+    fontSize: 150,
+    padding: 0,
   },
   button:{
     "background-color": 'Yellow',
+    // alignItems: "flex-end",
+    maxWidth: 150,
+    // pageBreakBefore: always,
+    "bottom": "1",
   }
 }))
 
@@ -76,18 +101,30 @@ function CounterButton(){
         <div>
           <CssBaseline/>
           <Container maxWidth="lg">
-          <Typography component="div" style={{backgroundColor:'cyan', height:'100vh'}} >
-         
-          <Card className={classes.root}>
-          {/* <ThemeProvider theme = {GeneralTheme}> */}
-          <p>You clicked {count} times</p>
-          <Button className = {classes.button} variant="contained" color="secondary" onClick={() => setCount(count + 1)}>
-            Click me
-          </Button>
-          {/* </ThemeProvider> */}
-          </Card>
-
-          </Typography>
+            <Typography component="div" style={{backgroundColor:'cyan', height:'100vh'}}className={classes.root}>
+          {/* <Typography className={classes.root} > */}
+          {/* <div className={classes.root}> */}
+              <Box sx={{ width: 500, 
+                          height: 500,
+                          bgcolor:"#26a69a",
+                          justifyContent:"center",
+                          alignItems: "center"}}>
+              <Card className={classes.cardCss}>
+              {/* <ThemeProvider theme = {GeneralTheme}> */}
+                {/* <p>You clicked {count} times</p>
+                <br/> */}
+                <Box sx={{padding:0}}>
+                <h1  className={classes.countDisplay}>{count}</h1>
+                </Box>
+                <Button className = {classes.button} variant="contained" color="secondary" onClick={() => setCount(count + 1)}>
+                  Click me
+                </Button>
+              {/* </ThemeProvider> */}
+              </Card>
+              </Box>
+            {/* </div> */}
+            {/* </Typography> */}
+            </Typography>
           </Container>
         </div>
       );
